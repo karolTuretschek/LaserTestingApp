af7ed683ce4a713e75b6c7ae5e089830bb5978ec
karolTuretschek <karolturetschek@gmail.com>
2024-02-05 20:46:57 +0000

    Add project files.

---
+++ LaserTestingApp/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OfficeOpenXml;
+
+namespace LaserTestingApp
+{
+    
+    public partial class MainWindow : Window
+    {
+        string filePath = "C:\\Users\\venqu\\OneDrive\\Dokumenty\\Honours\\DataLaserData.xlsx";
+
+        // ---------------------------------> Needing sorted into reading from the excel sheet
+
+        List<laserInfo> laserData = new List<laserInfo>{
+                new laserInfo { Time =  1, AmbientTemp = 0.00, UnitTemp = 0.00, Divergence = 1.00, PowerOutput = 5.00},
+            };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public class laserInfo
+        {
+
+            public int Time { get; set; }
+            public double AmbientTemp{ get; set; }
+            public double UnitTemp { get; set; }
+            public double Divergence { get; set; }
+            public double PowerOutput { get; set; }
+
+        }
+
+        private void LoadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Start of things");
+            string filePath = "C:\\Users\\venqu\\OneDrive\\Dokumenty\\Honours\\Data\\LaserData.xlsx";
+            try
+            {
+                
+                using (ExcelPackage package = new ExcelPackage(filePath))
+                {
+                    
+                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // Licence
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                    int rowCount = worksheet.Dimension.Rows;
+                    int columnCount = worksheet.Dimension.Columns;
+                    Debug.WriteLine("Rows: {0}", rowCount);
+                    for (int row = 1; row <= rowCount; row++)
+                    {
+                        for (int col = 1; col <= columnCount; col++)
+                        {
+                            var cellValue = worksheet.Cells[row, col].Text;
+                            Debug.WriteLine(cellValue + "\t");
+                        }
+                        //Debug.WriteLine("");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+    
+}
