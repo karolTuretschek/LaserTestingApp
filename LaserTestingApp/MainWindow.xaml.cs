d6cd8ad04ba5ad3ad94035c2c45e9f2de5e695aa
karolTuretschek <karolturetschek@gmail.com>
2024-02-12 19:17:06 +0000

    Data now writes into DataGrid from Excel Sheet

--- LaserTestingApp/MainWindow.xaml.cs
+++ LaserTestingApp/MainWindow.xaml.cs
@@ -11,25 +11,24 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Extensions.Primitives;
 using OfficeOpenXml;
+using static LaserTestingApp.MainWindow;
+using Plotly.NET;
 
 namespace LaserTestingApp
 {
-
+    
     public partial class MainWindow : Window
     {
+        
+
         string filePath = "C:\\Users\\venqu\\OneDrive\\Dokumenty\\Honours\\DataLaserData.xlsx";
-
-        // ---------------------------------> Needing sorted into reading from the excel sheet
-
-        List<laserInfo> laserData = new List<laserInfo>{
-                new laserInfo { Time =  1, AmbientTemp = 0.00, UnitTemp = 0.00, Divergence = 1.00, PowerOutput = 5.00},
-            };
-
 
         public MainWindow()
         {
@@ -38,44 +37,41 @@ namespace LaserTestingApp
         public class laserInfo
         {
 
-            public int Time { get; set; }
+            public double Time { get; set; }
             public double AmbientTemp { get; set; }
             public double UnitTemp { get; set; }
             public double Divergence { get; set; }
             public double PowerOutput { get; set; }
 
         }
-
-        private void LoadDataButton_Click(object sender, RoutedEventArgs e)
+        private List<laserInfo> laserInfos = new List<laserInfo>();
+        private void LoadDataButton_Click(object sender, RoutedEventArgs e) 
         {
-            Debug.WriteLine("Start of things");
             string filePath = "C:\\Users\\venqu\\OneDrive\\Dokumenty\\Honours\\Data\\LaserData.xlsx";
             LaserDataGrid.AutoGenerateColumns = false;
 
             try
             {
-
+                
                 using (ExcelPackage package = new ExcelPackage(filePath))
                 {
-                    LaserDataGrid.ItemsSource = LoadExcel();
+                    //LaserDataGrid.ItemsSource = LoadExcel(myData);
                     ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // Licence
                     ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-
-                    List<laserInfo> laserData = new List<laserInfo>();
-
                     int rowCount = worksheet.Dimension.Rows;
-                    int columnCount = worksheet.Dimension.Columns;
                     Debug.WriteLine("Rows: {0}", rowCount);
-                    for (int row = 1; row <= rowCount; row++)
-                    {
-                        for (int col = 1; col <= columnCount; col++)
-                        {
-                            var cellValue = worksheet.Cells[row, col].Text;
-                            Debug.WriteLine(cellValue + "\t");
-                            //Debug.WriteLine("");
-                        }
+                    var data = new List<laserInfo>();
+                    for (int row = 2; row <= 21; row++)
+                    {                       
+                        
+                        data = LoadExcel(double.Parse(worksheet.Cells[row , 1].Text),
+                                double.Parse((worksheet.Cells[row, 2].Text)),
+                                double.Parse((worksheet.Cells[row, 3].Text)),
+                                double.Parse((worksheet.Cells[row, 4].Text)),
+                                double.Parse((worksheet.Cells[row, 5].Text))
+                                ); // Insert data from rows into DataGrid
                     }
-
+                    LaserDataGrid.ItemsSource = data;
                 }
             }
             catch (Exception ex)
@@ -83,14 +79,13 @@ namespace LaserTestingApp
                 Debug.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
-        private List<laserInfo> LoadExcel()
+        private List<laserInfo> LoadExcel(double Time, double AmbientTemp, double UnitTemp, double Divergence, double PowerOutput)
         {
-            List<laserInfo> laserInfos = new List<laserInfo>();
+            
 
             laserInfos.Add(new laserInfo()
             {
-                Time =  1, AmbientTemp = 0.00, UnitTemp = 0.00, Divergence = 1.00, PowerOutput = 5.00
+                Time =  Time, AmbientTemp = AmbientTemp, UnitTemp = UnitTemp, Divergence = Divergence, PowerOutput = PowerOutput
             });
 
             return laserInfos;
